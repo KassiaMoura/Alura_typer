c6da6cdfdbd5487de213265c69b5c511a9d47aff
Kássia Moura <107736293+KassiaMoura@users.noreply.github.com>
2023-04-26 15:38:29 -0300

    Atualização do código com contador de tempo

--- Jquery/alura-typer/public/js/main.js
+++ Jquery/alura-typer/public/js/main.js
@@ -1,16 +1,24 @@
-var frase = $(".frase").text();
+var frase = $(".frase").text(); 
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
-var campo = $(".campo-digitacao")
-campo.on("input", function(){
-    var conteudo = campo.val();
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+var conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length -1;
-    $("#contador-palavras").text(qtdPalavras);
+var qtdPalavras = conteudo.split(/\S+/).length;
+ $("#contador-palavras").text(qtdPalavras);
 
-    var qtsCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtsCaracteres)
-    });
+var qtdCaracteres = conteudo.length;
+$("#contador-caracteres").text(qtdCaracteres);
 
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.on("focus", function() {
+    setInterval(function() {
+        tempoRestante--;
+        console.log(tempoRestante);
+    }, 1000);
+});
