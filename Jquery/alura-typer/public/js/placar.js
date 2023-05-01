66296b01525b1e5a66a77e2525286e340b3dbc98
Kássia Moura <107736293+KassiaMoura@users.noreply.github.com>
2023-05-01 03:40:20 -0300

    Correção no main e novos parâmetros no placar

--- Jquery/alura-typer/public/js/placar.js
+++ Jquery/alura-typer/public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Kássia"
@@ -7,10 +9,20 @@ function inserePlacar() {
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.prepend(linha);   
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+    $("html, body").animate(
+    {
+        scrollTop: posicaoPlacar + "px"
+    },1000);
 }
 
 function novaLinha(usuario,palavras){
-    var linha = $("<t>");
+    var linha = $("<tr>");
     var colunaUsuario = $("<td>").text(usuario);
     var colunaPalavras = $("<td>").text(palavras);
     var colunaRemover = $("<td>");
@@ -31,7 +43,14 @@ function novaLinha(usuario,palavras){
 
 function removeLinha(event){
     event.preventDefault();
-    $(this).parent().parent().remove();
+    var linha = $(this).parent().parent();
+    linha.fadeOut(1000);
+    setTimeout(function(){
+        linha.remove();
+    },1000);
+    
 }
 
-   
+function mostraPlacar(){
+    $(".placar").stop().slideToggle(600);
+}
